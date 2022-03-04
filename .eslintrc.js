24a67a39063ee9c8f99f0e106897c140b79c9ff1
Tomáš Martykán <t.martykan@gmail.com>
2022-03-04 19:31:55 +0100

    Fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "off",
     "prettier/prettier": "off",
   },
 };
